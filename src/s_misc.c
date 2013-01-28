37c7fbc6e51bdbf14115673e898e7a4629172a6a
canaan <canaan@tablomedia.com>
2013-01-28 15:55:22 -0500

    updating Disconnect log message in s_misc.c with decode_username

--- src/s_misc.c
+++ src/s_misc.c
@@ -576,14 +576,14 @@ int  exit_client(aClient *cptr, aClient *sptr, aClient *from, char *comment)
 			}
 			on_for = TStime() - sptr->firsttime;
 			if (IsHidden(sptr))
-				ircd_log(LOG_CLIENT, "Disconnect - (%ld:%ld:%ld) %s!%s@%s [VHOST %s]",
+				ircd_log(LOG_CLIENT, "Disconnect - (%ld:%ld:%ld) %s![%s]@%s [VHOST %s]",
 					on_for / 3600, (on_for % 3600) / 60, on_for % 60,
-					sptr->name, sptr->user->username,
+					sptr->name, decode_username(sptr->user->username),
 					sptr->user->realhost, sptr->user->virthost);
 			else
-				ircd_log(LOG_CLIENT, "Disconnect - (%ld:%ld:%ld) %s!%s@%s",
+				ircd_log(LOG_CLIENT, "Disconnect - (%ld:%ld:%ld) %s![%s]@%s",
 					on_for / 3600, (on_for % 3600) / 60, on_for % 60,
-					sptr->name, sptr->user->username, sptr->user->realhost);
+					sptr->name, decode_username(sptr->user->username), sptr->user->realhost);
 		} else
 		if (IsUnknown(sptr))
 		{
